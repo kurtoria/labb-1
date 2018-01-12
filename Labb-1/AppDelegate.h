6a4f6c8776fc6cb69257b79e83266036b3e09110
victoriagronqvist <victoriagronqvist@gmail.com>
2018-01-12 15:22:36 +0100

    Initial Commit

---
+++ Labb-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb-1
+//
+//  Created by Victoria Grönqvist on 2018-01-12.
+//  Copyright © 2018 Victoria Grönqvist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Labb-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb-1
+//
+//  Created by Victoria Grönqvist on 2018-01-12.
+//  Copyright © 2018 Victoria Grönqvist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
